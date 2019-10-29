406e98bb75a167e49bd6bb14c548694ee2249091
Cyril Pierron <cype59@gmail.com>
2019-10-29 21:58:39 +0100

    creation d'une boucle

--- js/script.js
+++ js/script.js
@@ -41,40 +41,17 @@ function getThreeDayForecast() {
       // Récupère la donnée d'une API
       const data = response.data;
 
-      // On récupère l'information principal
-      const mainTomorrow = data.list[1].weather[0].main;
-      const descriptionTomorrow = data.list[1].weather[0].description;
-      const tempTomorrow = data.list[1].temp.day;
-      const iconTomorrow = apiWeather.getHTMLElementFromIcon(data.list[1].weather[0].icon);
+      for (let i = 1; i < 5; i++) {
+        const main= data.list[i].weather[0].main;
+        const description = data.list[i].weather[0].description;
+        const temp = data.list[i].temp.day;
+        const icon= apiWeather.getHTMLElementFromIcon(data.list[i].weather[0].icon);
 
-      const mainAfterTomorrow = data.list[2].weather[0].main;
-      const descriptionAfterTomorrow = data.list[2].weather[0].description;
-      const tempAfterTomorrow = data.list[2].temp.day;
-      const iconAfterTomorrow = apiWeather.getHTMLElementFromIcon(data.list[2].weather[0].icon);
-
-      const mainAfterTomorrow2 = data.list[3].weather[0].main;
-      const descriptionAfterTomorrow2 = data.list[3].weather[0].description;
-      const tempAfterTomorrow2 = data.list[3].temp.day;
-      const iconAfterTomorrow2 = apiWeather.getHTMLElementFromIcon(data.list[3].weather[0].icon);
-
-
-      // Modifier le DOM
-      document.getElementById('tomorrow-forecast-main').innerHTML = mainTomorrow ;
-      document.getElementById('tomorrow-forecast-more-info').innerHTML = descriptionTomorrow;
-      document.getElementById('tomorrow-icon-weather-container').innerHTML = iconTomorrow;
-      document.getElementById('tomorrow-forecast-temp').innerHTML = `${tempTomorrow}°C`;
-
-      document.getElementById('after-tomorrow-forecast-main').innerHTML = mainAfterTomorrow ;
-      document.getElementById('after-tomorrow-forecast-more-info').innerHTML = descriptionAfterTomorrow;
-      document.getElementById('after-tomorrow-icon-weather-container').innerHTML = iconAfterTomorrow;
-      document.getElementById('after-tomorrow-forecast-temp').innerHTML = `${tempAfterTomorrow}°C`;
-
-      document.getElementById('after-tomorrow2-forecast-main').innerHTML = mainAfterTomorrow2 ;
-      document.getElementById('after-tomorrow2-forecast-more-info').innerHTML = descriptionAfterTomorrow2;
-      document.getElementById('after-tomorrow2-icon-weather-container').innerHTML = iconAfterTomorrow2;
-      document.getElementById('after-tomorrow2-forecast-temp').innerHTML = `${tempAfterTomorrow2}°C`;
-
-      
+        document.getElementById(`day-${i}-forecast-main`).innerHTML = main ;
+        document.getElementById(`day-${i}-forecast-more-info`).innerHTML = description;
+        document.getElementById(`day-${i}-icon-weather-container`).innerHTML = icon;
+        document.getElementById(`day-${i}-forecast-temp`).innerHTML = `${temp}°C`;
+      }      
     })
     .catch(function(error) {
       // Affiche une erreur
